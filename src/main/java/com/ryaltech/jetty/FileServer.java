22fa814e188b1645e71215ce257addc9dde32ee1
Alex Rykov <alex.rykov@gmail.com>
2011-06-16 10:39:06 -0400

    allows to specify a separate root directory

--- src/main/java/com/ryaltech/jetty/FileServer.java
+++ src/main/java/com/ryaltech/jetty/FileServer.java
@@ -7,14 +7,14 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
 
+import java.io.File;
+
 public class FileServer
 {
     public static void main(String[] args) throws Exception
     {
-    	
-    		
-    	
     	int port;
+		String webRoot = ".";
     	try{
     		port = Integer.parseInt(args[0]);
     	}catch(Exception ex){
@@ -22,6 +22,10 @@ public class FileServer
     		System.exit(-1);
     		return;
     	}
+		try{
+    		webRoot = args[1];
+    	}catch(Exception ex){		
+		}
     		
         Server server = new Server();
         SelectChannelConnector connector = new SelectChannelConnector();
@@ -32,18 +36,19 @@ public class FileServer
         resource_handler.setDirectoriesListed(false);
         resource_handler.setWelcomeFiles(new String[]{ "index.html" });
  
-        resource_handler.setResourceBase(".");
+        resource_handler.setResourceBase(webRoot);
  
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
         server.setHandler(handlers);
  
         server.start();
+		System.out.println("Web root is: "+new File(webRoot).getAbsolutePath());
         server.join();
     }
 
 	private static void help() {
-		System.out.println("Usage: java -jar jetty-static-web[version].jar <web server port>");
+		System.out.println("Usage: java -jar jetty-static-web[version].jar <web server port> [web content dir(defaults to work dir)]");
 
 		
 	}
